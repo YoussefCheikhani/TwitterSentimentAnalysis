f1f3dc3f72aae557f8de965574da8ba965bda2c7
YoussefCheikhani <76974490+YoussefCheikhani@users.noreply.github.com>
2022-12-31 00:20:09 +0100

    Retreive tweets before a certain date

--- stream_test.py
+++ stream_test.py
@@ -2,6 +2,7 @@ import tweepy
 from tweepy import OAuth2BearerHandler
 from tweepy import API
 from tweepy import Stream
+import datetime
 
 import twitter_credentials as tc
 
@@ -11,10 +12,12 @@ auth.set_access_token(tc.ACCESS_TOKEN, tc.ACCESS_TOKEN_SECRET)
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-print(public_tweets[0].text)
+search_term = 'world cup 2022'
+tweet_amount = 10
 
+tweets = []
+tmpTweets = tweepy.Cursor(method=api.search_tweets, q=search_term, lang='en',until="2022-12-23").items(tweet_amount)
 
+for tweet in tmpTweets:
+    print(tweet.created_at)
 
-
-#tc.API_KEY, tc.API_KEY_SECRET, tc.ACCESS_TOKEN, tc.ACCESS_TOKEN_SECRET
